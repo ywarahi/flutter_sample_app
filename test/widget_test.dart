540c8cb4a7f1dc853e0dcf905ac04cd66453d129
ywarahi <62178605+ywarahi@users.noreply.github.com>
2020-07-19 15:48:49 +0900

    split packages. and running OK.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_sample_app/original/MyApp.dart';
 
 import 'package:flutter_sample_app/main.dart';
 
